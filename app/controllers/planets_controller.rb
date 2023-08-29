71b3b4f20ded22bccd733e235f409f16b7d1cf97
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-08-29 17:50:19 +0400

    Added index and new

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,13 +1,9 @@
 class PlanetsController < ApplicationController
+  def index
+    @planets = Planet.all
+  end
+
   def new
-    # we have to locate which category the user will add the the planet
-    @category = Category.find(params[:category_id])
     @planet = Planet.new
   end
-
-  def create
-    @category = Category.find(params[:category_id])
-    
-  end
-
 end
