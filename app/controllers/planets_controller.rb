1d19a12de1a4b8923aa379e8e1462438f92a64f1
Hai Dinh NGUYEN <sandersonns@gmail.com>
2023-08-29 15:58:32 +0200

    works on create feature

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -5,9 +5,21 @@ class PlanetsController < ApplicationController
     @planet = Planet.new
   end
 
-  def create
-    @category = Category.find(params[:category_id])
-    
+  def index
+    @planet = Planet.all
   end
 
+  def create
+    @planet = Planet.new(planet_params)
+    if @planet.save
+      redirect_to @planet, notice: "A planet was successfully added."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def planet_params
+    params.require(:planet).permit(:title, :description, :renting_price, :image_url)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,30 @@
-# category_names = [
-#   "Terrestrial Planets",
-#   "Gas Giant Planets",
-#   "Dwarf Planets",
-#   "Exoplanets",
-#   "Planetary Moons",
-#   "Historical Planets",
-#   "Planetary Exploration",
-#   "Planetary Atmospheres",
-#   "Planetary Features",
-#   "Planet Formation"
-# ]
+puts "Cleaning database..."
+Category.destroy_all
+Planet.destroy_all
+
+puts "Generating planet categories"
+
+category_names = [
+  "Terrestrial Planets",
+  "Gas Giant Planets",
+  "Dwarf Planets",
+  "Exoplanets",
+  "Planetary Moons",
+  "Historical Planets",
+  "Planetary Exploration",
+  "Planetary Atmospheres",
+  "Planetary Features",
+  "Planet Formation"
+]
 
 
-# category_names.each do |name|
-#   Category.create(name: name)
-# end
+category_names.each do |name|
+  Category.create(name: name)
+end
+
+puts "Generating planets"
+
+fist_planet = Planet.create(title: "Earth", description: "Habitable", renting_price: 123.5, image_url: "abc" )
+second_planet = Planet.create(title: "Mars", description: "Unhabitable", renting_price: 23.5, image_url: "abcd" )
+
+puts "Finished generating planets"
