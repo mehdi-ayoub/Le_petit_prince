1712506af08c13fade77cff47f3b672072d1f9a8
Hai Dinh NGUYEN <sandersonns@gmail.com>
2023-08-29 18:04:14 +0200

    create feature and other things

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,12 +7,13 @@ class PlanetsController < ApplicationController
     @planet = Planet.new
   end
 
-  def index
-    @planet = Planet.all
+  def show
   end
 
   def create
     @planet = Planet.new(planet_params)
+    @planet.category = Category.find(params[:planet][:category_id])
+    @planet.user = current_user
     if @planet.save
       redirect_to @planet, notice: "A planet was successfully added."
     else
@@ -20,8 +21,32 @@ class PlanetsController < ApplicationController
     end
   end
 
+  #Haya B
+  def edit
+    @planet = Planet.find(params[:id])
+  end
+
+  def update
+    @planet = Planet.find(params[:id])
+    if @planet.update(planet_params)
+      redirect_to(@planet)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  #Joshua
+  def destroy
+    @planet = Planet.find(params[:id])
+    if @planet.destroy
+      redirect_to planets_path(@planet)
+    else
+      render :index
+    end
+  end
+
   private
   def planet_params
-    params.require(:planet).permit(:title, :description, :renting_price, :image_url)
+    params.require(:planet).permit(:title, :description, :renting_price, :image_url, :category_id)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :planets
+  has_many :planets, dependent: :destroy
 end
