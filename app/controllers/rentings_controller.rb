c336b85a70ce1a006eaeb836552aeeff02ca7f49
Josua Hasler <josua.hasler@hotmail.com>
2023-08-31 16:38:42 +0200

    finalized accept-decline

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -26,19 +26,21 @@ class RentingsController < ApplicationController
 
   def my_offers
     @rentings = current_user.rentings_as_owner
+    @rentings = @rentings.order(:id)
   end
 
   def accept
     @renting = Renting.find(params[:id])
+    puts @renting
     @renting.status = "accepted"
-    @renting.status.save
+    @renting.save
     redirect_to my_offers_rentings_path, status: :see_other
   end
 
   def decline
     @renting = Renting.find(params[:id])
     @renting.status = "declined"
-    @renting.status.save
+    @renting.save
     redirect_to my_offers_rentings_path, status: :see_other
   end
 
